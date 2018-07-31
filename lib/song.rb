2cb43b775b8fd7c7bf9ddbbad2c4ad6b2ffc0eb3
Amee Covarrubias <ameecova@yahoo.com>
2018-07-31 02:58:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.find_by_name(name)
     song = self.new
     song.name = name 
-    @@all.detect(name)
+    @@all.detect{|song| song.name == name}
   end 
   
   def self.find_by_name(name)
