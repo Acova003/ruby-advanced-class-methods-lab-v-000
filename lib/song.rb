789a767006000e0ad38dc40248cc895502f2daf1
Amee Covarrubias <ameecova@yahoo.com>
2018-07-31 03:33:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,8 @@ class Song
   def self.create_from_filename(filename)
     name_array = filename.chomp(".mp3").split(" - ")
     song = Song.new 
-    song.name = split_filename[0]
+    song.name = name_array[1]
+    song.artist_name = name_array[0]
     song.save
     song
   end 
